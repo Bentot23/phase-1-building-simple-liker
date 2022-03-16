8f271d282850b7efd342c53504dd85ab87784341
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-03-16 14:14:46 -0500

    commit tree 41a6eeed606f5090df541b726c9ddcde28e2d6ae

--- main.js
+++ main.js
@@ -2,10 +2,42 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
+
 // Your JavaScript code goes here!
+const heartLike = document.querySelectorAll('.like-glyph')
+// const errorModal = document.querySelector("#modal")
 
+document.addEventListener("DOMContentLoaded", () => {
+  // console.log('DOM Content is Loaded')
+  // errorModal.classList.add('hidden')
+  updateHeart()
+})
 
+function hideError() {
+  errorModal.classList.add('hidden')
+}
 
+function updateHeart() {
+    // console.log(heartLike)
+    heartLike.forEach((like) => {
+      // console.log('LIKE', like)
+      like.addEventListener('click', () => {
+        mimicServerCall()
+        .then(resp => {
+          console.log(resp)
+          like.classList.add('activated-heart')
+        })
+        .catch((error) => {
+          console.log(error)
+          console.log(EMPTY_HEART)
+          errorModal.classList.remove('hidden')
+          like.classList.remove('activated-heart')
+        })
+          setTimeout(hideError, 3000)
+      })
+    })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
